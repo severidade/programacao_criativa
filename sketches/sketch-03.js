2bec37c9d148998d81fc454db0849015974d80d5
severo <oliveira.severo@gmail.com>
2023-01-03 18:32:18 -0300

    feat: :rocket: Criando dois objetos

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -9,10 +9,16 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const point = new Point(800, 400, 10);
+    const pointA = new Point(800, 400, 10);
+    const pointB = new Point(300, 700, 10);
 
     context.beginPath();
-    context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
+    context.arc(pointA.x, pointA.y, pointA.radius, 0, Math.PI * 2);
+    context.fillStyle = "black";
+    context.fill();
+
+    context.beginPath();
+    context.arc(pointB.x, pointB.y, pointB.radius, 0, Math.PI * 2);
     context.fillStyle = "black";
     context.fill();
   };
