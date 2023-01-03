c7b306f7f58201e0ed61254f130e2b77ce0b1fd1
severo <oliveira.severo@gmail.com>
2023-01-03 18:29:42 -0300

    feat: :rocket: Criando o terceiro sketch
    Nele há um ponto feito com uma classe

---
+++ sketches/sketch-03.js
@@ -0,0 +1,30 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    const point = new Point(800, 400, 10);
+
+    context.beginPath();
+    context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
+    context.fillStyle = "black";
+    context.fill();
+  };
+};
+
+canvasSketch(sketch, settings);
+
+class Point {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+
+}
