46802294b8d6b07ba881447bb2ec3370ba5833e7
severo <oliveira.severo@gmail.com>
2023-01-04 23:01:40 -0300

    feat: :rocket: Animando o desenho

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -1,12 +1,14 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
@@ -30,9 +32,12 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
-      const n = random.noise2D(x, y);
-      const angle = n * Math.PI;
+      const n = random.noise2D(x  + frame * 20, y, 0.001);
+      const angle = n * Math.PI * 0.2;
 
+      // const scale = (n + 1) / 2 * 30;
+      // const scale = (n * 0.5 + 0.5) * 30;
+      const scale = math.mapRange(n, -1, 1, 1, 30)
 
 
       context.save();
@@ -41,7 +46,7 @@ const sketch = () => {
       context.translate(cellw * 0.5, cellh * 0.5);
       context.rotate(angle);
 
-      context.lineWidth = 4;
+      context.lineWidth = scale;
 
       context.beginPath();
       context.moveTo( w * -0.5, 0);
